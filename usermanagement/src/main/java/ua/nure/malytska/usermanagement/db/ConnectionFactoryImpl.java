2fdbe6d595ef366ad399a38d6b68d40c63a1ab84
nadya <nadiia.malytska@nure.ua>
2019-10-29 09:32:14 +0200

    add connection factory

---
+++ usermanagement/src/main/java/ua/nure/malytska/usermanagement/db/ConnectionFactoryImpl.java
@@ -0,0 +1,26 @@
+package ua.nure.malytska.usermanagement.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+    private String driver = "org.hsqldb.jdbcDriver";
+    private String url = "jdbc:hsqldb:file:db/usermanagement";
+    private String user = "sa";
+    private String password = "";
+
+    @Override
+    public Connection createConnection() throws DatabaseException {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
+    }
+}
--- usermanagement/src/main/java/ua/nure/malytska/usermanagement/db/DatabaseException.java
+++ usermanagement/src/main/java/ua/nure/malytska/usermanagement/db/DatabaseException.java
@@ -1,5 +1,10 @@
 package ua.nure.malytska.usermanagement.db;
 
+import java.sql.SQLException;
+
 public class DatabaseException extends Exception {
 
+    public DatabaseException(Exception e) {
+        super(e);
+    }
 }
